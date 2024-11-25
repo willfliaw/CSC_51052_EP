79b4f12059fca146d37308a06eac2e3a698eef50
William Liaw <willfliaw@gmail.com>
2024-11-25 20:02:48 +0100

    improved choropleth

--- Project/main/js/choropleth.js
+++ Project/main/js/choropleth.js
@@ -1,21 +1,17 @@
 const ctx = {
     MAP_W: 960,
-    MAP_H: 500,
-    countryCodeTranslation: {},
+    MAP_H: 550,
+    isChoropleth: true,
 };
 
-let originalGeoData = null;
-let isChoropleth = true;
-let athletesByCountry = {};
-
-function drawLegend(colorScale, maxCount) {
+function drawLegend(svgEl, maxCount) {
     const legendWidth = 300;
     const legendHeight = 20;
-    const legendMargin = { top: -20, right: 10, bottom: 30, left: 10 };
+    const legendMargin = { top: -50, right: 10, bottom: 30, left: 10 };
 
-    const legendSvg = d3
-        .select("svg")
+    const legendSvg = svgEl
         .append("g")
+        .attr("id", "legend")
         .attr("class", "legend")
         .attr(
             "transform",
@@ -36,9 +32,9 @@ function drawLegend(colorScale, maxCount) {
     gradient
         .selectAll("stop")
         .data(
-            colorScale.ticks(10).map((t, i, arr) => ({
+            ctx.colorScale.ticks(10).map((t, i, arr) => ({
                 offset: `${(i / (arr.length - 1)) * 100}%`,
-                color: colorScale(t),
+                color: ctx.colorScale(t),
             }))
         )
         .enter()
@@ -83,156 +79,171 @@ function drawLegend(colorScale, maxCount) {
         .text("Number of Athletes");
 }
 
+function handleMouseOut() {
+    ctx.tooltip.transition().duration(500).style("opacity", 0);
+}
+
 function updateMap() {
+    const svgEl = d3.select("svg");
     const selectedContinent = document.getElementById("continentSelect").value;
 
-    let filteredGeoData = originalGeoData;
+    let filteredGeoData = ctx.originalGeoData;
     if (selectedContinent !== "All") {
         filteredGeoData = {
-            ...originalGeoData,
-            features: originalGeoData.features.filter(
+            ...ctx.originalGeoData,
+            features: ctx.originalGeoData.features.filter(
                 (d) => d.properties.continent === selectedContinent
             ),
         };
     }
 
-    drawChoropleth(filteredGeoData);
-    if (!isChoropleth) {
-        drawBubbleMap(filteredGeoData);
+    const projection = d3
+        .geoNaturalEarth1()
+        .fitSize([ctx.MAP_W, ctx.MAP_H - 60], filteredGeoData);
+
+    const geoPath = d3.geoPath().projection(projection);
+
+    ctx.tooltip = d3
+        .select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("padding", "8px")
+        .style("background", "rgba(0, 0, 0, 0.8)")
+        .style("color", "white")
+        .style("border-radius", "4px")
+        .style("pointer-events", "none")
+        .style("opacity", 0);
+
+    const handleMouseOverMap = (event, d) => {
+        const count = ctx.countryData[d.id]?.count || 0;
+        ctx.tooltip.transition().duration(200).style("opacity", 0.9);
+        ctx.tooltip
+            .html(`<strong>${ctx.countryData[d.id]?.full_name}</strong><br>Athletes: ${count}`)
+            .style("left", `${event.pageX + 10}px`)
+            .style("top", `${event.pageY - 28}px`);
+    };
+
+    svgEl.select("#map").remove();
+
+    svgEl
+        .insert("g", ":first-child")
+        .attr("id", "map")
+        .selectAll("path")
+        .data(filteredGeoData.features)
+        .enter()
+        .append("path")
+        .attr("d", geoPath)
+        .attr("fill", ctx.colorScale(0))
+        .attr("stroke", "#000")
+        .on("mouseover", handleMouseOverMap)
+        .on("mouseout", handleMouseOut);
+
+    if (ctx.isChoropleth) {
+        drawChoropleth(svgEl);
+    } else {
+        drawBubbleMap(svgEl, projection, filteredGeoData);
     }
 }
 
 function toggleView() {
-    isChoropleth = !isChoropleth;
-    document.getElementById("toggleView").innerText = isChoropleth
+    ctx.isChoropleth = !ctx.isChoropleth;
+    document.getElementById("toggleView").innerText = ctx.isChoropleth
         ? "Switch to Bubble Map"
         : "Switch to Choropleth";
     updateMap();
 }
 
-function drawBubbleMap(geoData) {
-    let projection = d3
-        .geoIdentity()
-        .reflectY(true)
-        .fitSize([ctx.MAP_W, ctx.MAP_H], geoData);
+function drawBubbleMap(svgEl, projection, filteredGeoData) {
+    const handleMouseOverBubble = (event, d) => {
+        ctx.tooltip.transition().duration(200).style("opacity", 0.9);
+        ctx.tooltip
+            .html(`<strong>${d.full_name}</strong><br>Athletes: ${d.count}`)
+            .style("left", `${event.pageX + 10}px`)
+            .style("top", `${event.pageY - 28}px`);
+    };
 
-    const maxCount = d3.max(Object.values(athletesByCountry)) || 1;
-    ctx.sizeScale = d3.scaleSqrt().domain([0, maxCount]).range([0, 30]);
+    const filteredCountryData = Object.fromEntries(
+        Object.entries(ctx.countryData).filter(([key, value]) =>
+            filteredGeoData.features.some((feature) => feature.id === key)
+        )
+    );
 
-    console.log(geoData);
-
-    const athletesArray = Object.entries(athletesByCountry)
-        .map(([country_3_letter_code, count]) => {
-            const country = ctx.geoDataCenter.find(
-                (c) =>
-                    c.short_name ===
-                    ctx.countryCodeTranslation[country_3_letter_code]
-            );
-
-            const isIdInAdditionalArray = geoData.features.some(
-                (obj) => obj.id === country_3_letter_code
-            );
-
-            if (country && isIdInAdditionalArray) {
-                return {
-                    name: country.long_name,
-                    count,
-                    lat: +country.center_lat,
-                    lon: +country.center_lng,
-                };
-            }
-            return null;
-        })
-        .filter(Boolean);
-
-    const bubbles = d3
-        .select("svg")
+    const bubbles = svgEl
+        .append("g")
+        .attr("id", "bubbles")
         .selectAll("circle")
-        .data(athletesArray)
+        .data(Object.values(filteredCountryData), (d) => d.country_code);
+
+    bubbles
         .enter()
         .append("circle")
+        .on("mouseover", handleMouseOverBubble)
+        .on("mouseout", handleMouseOut)
+        .attr("id", (d) => `bubble_${d.country_code}`)
         .attr("class", "bubble")
         .attr("cx", (d) => projection([d.lon, d.lat])[0])
         .attr("cy", (d) => projection([d.lon, d.lat])[1])
         .attr("fill", "steelblue")
-        .attr("r", ctx.sizeScale(0));
-
-    bubbles
+        .attr("r", 0)
         .transition()
+        .delay((_, i) => i * 50)
         .duration(1000)
         .ease(d3.easeElasticOut)
         .attr("r", (d) => ctx.sizeScale(d.count));
 
-    bubbles.on("mouseover", function (event, d) {
-        const tooltip = d3.select("#tooltip");
-        tooltip
-            .style("opacity", 1)
-            .style("left", `${event.pageX + 10}px`)
-            .style("top", `${event.pageY + 10}px`)
-            .html(`<strong>${d.name}</strong><br>Value: ${d.count}`);
-    });
-
-    bubbles.on("mouseout", function () {
-        d3.select("#tooltip").style("opacity", 0);
-    });
-}
-
-function drawChoropleth(geoData) {
-    d3.select("#mapContainer").html("");
-    const svg = d3
-        .select("#mapContainer")
-        .append("svg")
-        .attr("width", ctx.MAP_W)
-        .attr("height", ctx.MAP_H);
-
-    const maxCount = d3.max(Object.values(athletesByCountry)) || 1;
-    const colorScale = d3
-        .scaleSequential(d3.interpolateReds)
-        .domain([0, maxCount]);
-    const tooltip = d3.select("#tooltip");
-
-    let projection = d3
-        .geoIdentity()
-        .reflectY(true)
-        .fitSize([ctx.MAP_W, ctx.MAP_H], geoData);
-
-    const geo_path = d3.geoPath().projection(projection);
-
-    svg.selectAll("*").remove();
-
-    svg.append("g")
+    svgEl.select("#legend").transition().duration(500).style("opacity", 0);
+    svgEl
         .selectAll("path")
-        .data(geoData.features)
-        .enter()
-        .append("path")
-        .attr("d", geo_path)
-        .attr("fill", (d) => {
-            const count = athletesByCountry[d.id] || 0;
-            return colorScale(count);
-        })
-        .attr("stroke", "#000")
-        .on("mouseover", (event, d) => {
-            const count = athletesByCountry[d.id] || 0;
-            tooltip.transition().duration(200).style("opacity", 0.9);
-            tooltip
-                .html(
-                    `<strong>${d.properties.name}</strong><br>Athletes: ${count}`
-                )
-                .style("left", `${event.pageX + 10}px`)
-                .style("top", `${event.pageY - 28}px`);
-        })
-        .on("mouseout", () => {
-            tooltip.transition().duration(500).style("opacity", 0);
-        });
-
-    drawLegend(colorScale, maxCount);
+        .transition()
+        .duration(500)
+        .attr("fill", ctx.colorScale(0));
 }
 
-function enrichGeoDataWithContinents(geoData, isoData) {
+function drawChoropleth(svgEl) {
+    const drawMap = () => {
+        svgEl
+            .selectAll("path")
+            .transition()
+            .duration(500)
+            .attr("fill", (d) => {
+                const count = ctx.countryData[d.id]?.count || 0;
+                return ctx.colorScale(count);
+            });
+
+        svgEl
+            .select("#legend")
+            .raise()
+            .transition()
+            .duration(500)
+            .style("opacity", 1);
+    };
+
+    const removeCirclesAndDrawMap = () => {
+        const circles = svgEl.selectAll("circle");
+
+        if (!circles.empty()) {
+            circles
+                .transition()
+                .duration(500)
+                .attr("r", ctx.sizeScale(0))
+                .on("end", () => {
+                    svgEl.select("#bubbles").remove();
+                    drawMap();
+                });
+        } else {
+            drawMap();
+        }
+    };
+
+    removeCirclesAndDrawMap();
+}
+
+function enrichGeoDataWithContinents(geoData, regionData) {
     const isoToContinent = {
         SDS: "Africa",
     };
-    isoData.forEach((row) => {
+    regionData.forEach((row) => {
         isoToContinent[row["alpha-3"]] = row["region"];
     });
 
@@ -242,13 +253,38 @@ function enrichGeoDataWithContinents(geoData, isoData) {
     });
 }
 
-function loadData() {
+function transformData(data) {
+    const countryData = {};
+
+    data.forEach((row) => {
+        const countryKey = row.country_3_letter_code;
+
+        if (!countryData[countryKey]) {
+            const centroid = d3.geoCentroid(
+                ctx.originalGeoData.features.find(
+                    (country) => country.id === row.country_3_letter_code
+                )
+            );
+
+            countryData[countryKey] = {
+                country_code: row.country_code,
+                full_name: row.first_game_country,
+                count: 0,
+                lon: centroid ? centroid[0] : null,
+                lat: centroid ? centroid[1] : null,
+            };
+        }
+
+        countryData[countryKey].count += 1;
+    });
+
+    return countryData;
+}
+
+function loadData(svgEl) {
     let promises = [
         d3.json(
             "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-        ),
-        d3.json(
-            "https://raw.githubusercontent.com/mihai-craita/countries_center_box/refs/heads/master/countries.json"
         ),
         d3.csv(
             "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/refs/heads/master/all/all.csv"
@@ -256,24 +292,23 @@ function loadData() {
         d3.csv("data/clean/olympic_athletes.csv"),
     ];
 
-    Promise.all(promises).then(([geoData, geoDataCenter, isoData, data]) => {
-        originalGeoData = geoData;
-        ctx.geoDataCenter = geoDataCenter;
+    Promise.all(promises).then(([geoData, regionData, data]) => {
+        ctx.originalGeoData = geoData;
 
-        enrichGeoDataWithContinents(geoData, isoData);
+        enrichGeoDataWithContinents(geoData, regionData);
 
-        data.forEach((d) => {
-            const country_3_letter_code = d.country_3_letter_code;
-            const country_code = d.country_code;
-            athletesByCountry[country_3_letter_code] =
-                (athletesByCountry[country_3_letter_code] || 0) + 1;
+        ctx.countryData = transformData(data);
 
-            if (!(country_3_letter_code in ctx.countryCodeTranslation)) {
-                ctx.countryCodeTranslation[country_3_letter_code] =
-                    country_code;
-            }
-        });
+        const maxCount = d3.max(
+            Object.values(ctx.countryData).map((item) => item.count)
+        );
 
+        ctx.colorScale = d3
+            .scaleSequential(d3.interpolateReds)
+            .domain([0, maxCount]);
+        ctx.sizeScale = d3.scaleSqrt().domain([0, maxCount]).range([0, 30]);
+
+        drawLegend(svgEl, maxCount);
         updateMap();
     });
 }
@@ -281,5 +316,10 @@ function loadData() {
 function createViz() {
     console.log("Using D3 v" + d3.version);
     document.getElementById("continentSelect").value = "All";
-    loadData();
+    const svgEl = d3
+        .select("#mapContainer")
+        .append("svg")
+        .attr("width", ctx.MAP_W)
+        .attr("height", ctx.MAP_H);
+    loadData(svgEl);
 }
